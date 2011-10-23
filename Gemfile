618625dc580ef4dcf52fb4facf04ec6b77e66383
bbarr <brendanbarr.web@gmail.com>
2011-10-23 15:33:20 -0400

    restructured to run off of ruby while developing for eventual phonegap

---
+++ Gemfile
@@ -0,0 +1,8 @@
+source "http://rubygems.org"
+
+gem "mongo_mapper"
+gem "sinatra"
+gem "rack-contrib"
+gem "json"
+gem "shotgun", :group => :development
+gem "rspec", :group => :development
---
+++ app.rb
@@ -0,0 +1,23 @@
+require "rubygems"
+require "bundler"
+Bundler.require
+
+get "/" do
+  "hi"
+end
+
+get "/beers" do
+  
+end
+
+post "/locations/" do
+  
+end
+
+get "/locations/:id" do
+  
+end
+
+post "/locations/:id/beers" do
+  
+end
